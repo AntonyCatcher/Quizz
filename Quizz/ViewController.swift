e56c5c797079b789a58d12a2b451d076adde5f33
AntonyCatcher <anton.chvertkov@yahoo.com>
2023-02-01 13:04:33 +0300

    Initial Commit

---
+++ Quizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizz
+//
+//  Created by Anton  on 01.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
