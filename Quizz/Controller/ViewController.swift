d8c1e32b594df393686ca6f75a79e00b1b6da5b7
AntonyCatcher <anton.chvertkov@yahoo.com>
2023-02-13 22:02:09 +0300

    true/false implementation

---
+++ Quizz/Controller/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Quizz
+//
+//  Created by Anton  on 01.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    var quizBrain = QuizBrain()
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        guard let userAnswer = sender.currentTitle else { return }
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.systemGreen
+        } else {
+            sender.backgroundColor = UIColor.systemRed
+        }
+        
+        quizBrain.nextQuestion()
+
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+}
+
---
+++ Quizz/Model/Questions.swift
@@ -0,0 +1,18 @@
+//
+//  Questions.swift
+//  Quizz
+//
+//  Created by Anton  on 13.02.2023.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+         text = q
+         answer = a
+    }
+}
